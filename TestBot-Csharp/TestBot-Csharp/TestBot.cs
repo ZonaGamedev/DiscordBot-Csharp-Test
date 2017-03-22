9face92bc0157f235889c77504040325058b0e69
antonventuro <anton.venturo@hotmail.com>
2017-03-22 04:15:44 -0500

    Creando Clases

---
+++ TestBot-Csharp/TestBot-Csharp/TestBot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace TestBot_Csharp
+{
+    class TestBot
+    {
+    }
+}
