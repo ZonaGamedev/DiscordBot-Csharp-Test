4e31dfb0a540368122d7fd343cb5a5feec449358
markoantoniov <marko_antoniov@hotmail.com>
2017-04-01 04:34:47 +0200

    Control de eventos básicos de ingreso y despedida del servidor
    Se ha integrado la escucha para los eventos básicos de ingreso y
    despedida del servidor a modo de test para poder mejorar y refactorizar
    el código.

--- TestBot-Csharp/TestBot-Csharp/Program.cs
+++ TestBot-Csharp/TestBot-Csharp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Modules;
+using TestBot_Csharp.Utils;
 
 namespace TestBot_Csharp
 {
@@ -10,6 +12,41 @@ namespace TestBot_Csharp
     {
         static void Main(string[] args)
         {
+            DiscordClient _client;
+            _client = new DiscordClient();
+
+            // Eventos básicos de ingreso y despedida del servidor
+            _client.UserJoined += async (s, e) =>
+            {
+                await Utils.MensajesLog.LogEntradaServidor(e.User.Name, Utils.MensajesLog.Soporte.consola);
+            };
+            _client.UserLeft += async (s, e) =>
+            {
+                await Utils.MensajesLog.LogSalidaServidor(e.User.Name, Utils.MensajesLog.Soporte.consola);
+            };
+
+            //TODO: Pendiente de encontrar los eventos de login y logout en el servidor
+            _client.JoinedServer += async (s, e) =>
+            {
+                await Utils.MensajesLog.LogUsrConectado(e.Server.CurrentUser.Name, Utils.MensajesLog.Soporte.consola);
+            };
+            _client.LeftServer += async (s, e) =>
+            {
+                await Utils.MensajesLog.LogUsrDesconectado(e.Server.CurrentUser.Name, Utils.MensajesLog.Soporte.consola);
+            };
+            //----------------------------
+
+
+            //_client.MessageReceived += async (s, e) =>
+            //{
+            //    if (!e.Message.IsAuthor)
+            //        await e.Channel.sendmessage(e.message.text);
+            //};
+
+            _client.ExecuteAndWait(async () =>
+            {
+                await _client.Connect(ConfigurationManager.AppSettings["tokenBot"], TokenType.Bot);
+            });
         }
     }
 }
---
+++ TestBot-Csharp/TestBot-Csharp/Utils/MensajesLog.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot_Csharp.Utils
+{
+    public static class MensajesLog
+    {
+        public enum Soporte
+        {
+            consola,
+            baseDeDatos
+        }
+
+        public static async Task<Boolean> LogEntradaServidor(string usuario, Soporte soporte)
+        {
+            await Task.Run(() =>
+            {
+                switch (soporte)
+                {
+                    case Soporte.consola:
+                        Console.Write(usuario + " ha entrado en el servidor\n");
+                        break;
+                    case Soporte.baseDeDatos:
+                        break;
+                }
+            });
+
+            return true;
+        }
+
+        public static async Task<Boolean> LogSalidaServidor(string usuario, Soporte soporte)
+        {
+            await Task.Run(() =>
+            {
+                switch (soporte)
+                {
+                    case Soporte.consola:
+                        Console.Write(usuario + " ha salido del servidor\n");
+                        break;
+                    case Soporte.baseDeDatos:
+                        break;
+                }
+            });
+
+            return true;
+        }
+
+        public static async Task<Boolean> LogUsrConectado(string usuario, Soporte soporte)
+        {
+            await Task.Run(() =>
+            {
+                switch (soporte)
+                {
+                    case Soporte.consola:
+                        Console.Write(usuario + " se ha conectado\n");
+                        break;
+                    case Soporte.baseDeDatos:
+                        break;
+                }
+            });
+
+            return true;
+        }
+
+        public static async Task<Boolean> LogUsrDesconectado(string usuario, Soporte soporte)
+        {
+            await Task.Run(() =>
+            {
+                switch (soporte)
+                {
+                    case Soporte.consola:
+                        Console.Write(usuario + " se ha desconectado\n");
+                        break;
+                    case Soporte.baseDeDatos:
+                        break;
+                }
+            });
+
+            return true;
+        }
+    }
+}
